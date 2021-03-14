91985561c25d7e8c87db0ff10c8d07a338d1cf22
helchall <h_el_chall@hotmail.com>
2021-03-14 14:47:55 +0100

    Adding dashboard controller and template

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/dashboard")
+ */
+class DashboardController extends AbstractController
+{
+    /**
+     * @Route("/", name="dashboard_index", methods={"GET"})
+     */
+    public function index(Request $request): Response
+    {
+        return $this->render('dashboard/index.html.twig');
+    }
+}
