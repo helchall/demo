007b159c198d01ba057f7a1aff7f38d6b4673028
helchall <h_el_chall@hotmail.com>
2021-03-14 15:32:44 +0100

    Updating dashboard controller

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\CarteRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,8 +17,16 @@ class DashboardController extends AbstractController
     /**
      * @Route("/", name="dashboard_index", methods={"GET"})
      */
-    public function index(Request $request): Response
+    public function index(
+        UserRepository $userRepository,
+        CarteRepository $carteRepository
+    ): Response
     {
-        return $this->render('dashboard/index.html.twig');
+        // Get users
+
+        return $this->render('dashboard/index.html.twig', [
+            'users_count' => count($userRepository->findAll()),
+            'cards_count' => count($carteRepository->findAll()),
+        ]);
     }
 }
