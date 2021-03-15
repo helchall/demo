b5636e5c128ad8f6280a7900d45c170109593781
helchall <h_el_chall@hotmail.com>
2021-03-15 17:03:57 +0100

    Adding registration template and modifying login template

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,10 +4,13 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
+use App\Repository\UserRepository;
+use App\Security\LoginFormAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
 /**
  * @Route("/register")
@@ -17,8 +20,13 @@ class RegistrationController extends AbstractController
     /**
      * @Route("/", name="user_registration")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
-    {
+    public function register(
+        Request $request,
+        UserPasswordEncoderInterface $passwordEncoder,
+        GuardAuthenticatorHandler $guardHandler,
+        LoginFormAuthenticator $authenticator,
+        \Swift_Mailer $mailer
+    ){
         // 1) build the form
         $user = new User();
         $form = $this->createForm(UserType::class, $user, [
@@ -32,6 +40,9 @@ class RegistrationController extends AbstractController
             $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
             $user->setPassword($password);
 
+            // Generate activiation token
+            $user->setActivationToken(md5(uniqid()));
+
             // 4) save the User!
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
@@ -40,12 +51,65 @@ class RegistrationController extends AbstractController
             // ... do any other work - like sending them an email, etc
             // maybe set a "flash" success message for the user
 
-            return $this->redirectToRoute('app_login');
+            // do anything else you need here, like send an email
+            // @todo create a service for sending mail ... to remove !
+            // On crée le message
+            $message = (new \Swift_Message('Nouveau compte'))
+            // On attribue l'expéditeur
+            ->setFrom('votre@adresse.fr')
+            // On attribue le destinataire
+            ->setTo($user->getEmail())
+            // On crée le texte avec la vue
+            ->setBody(
+                $this->renderView(
+                    'emails/activation.html.twig', ['token' => $user->getActivationToken()]
+                ),
+                'text/html'
+            )
+            ;
+            $mailer->send($message);
+
+            // Connect user if success
+            return $guardHandler->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $authenticator,
+                'main' // firewall name in security.yaml
+            );
+
+            //return $this->redirectToRoute('app_login');
         }
 
         return $this->render(
             'registration/register.html.twig',
             array('form' => $form->createView())
         );
+    }
+
+    /**
+     * @Route("/activation/{token}", name="activation")
+     */
+    public function activation($token, UserRepository $userRepository)
+    {
+        // Find if a user exists with this token in datavase
+        $user = $userRepository->findOneBy(['activationToken' => $token]);
+
+        // if any user exists
+        if(!$user){
+            // Send 404 error
+            throw $this->createNotFoundException('This user does not exists');
+        }
+
+        // Delete token
+        $user->setActivationToken(null);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        // Send flash message
+        $this->addFlash('message', 'User activated with success');
+
+        // Redirect to dashboard page
+        return $this->redirectToRoute('dashboard');
     }
 }
