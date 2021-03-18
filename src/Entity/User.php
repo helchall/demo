e20ade16d662b87e294447488da867594470eb64
helchall <h_el_chall@hotmail.com>
2021-03-18 15:26:25 +0100

    Updating user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -155,4 +155,11 @@ class User implements UserInterface
 
         return $this;
     }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
 }
