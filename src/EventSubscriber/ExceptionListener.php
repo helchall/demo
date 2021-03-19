2c752b5f426e4fdc19e8c0356c9b8d78da9e28c7
helchall <h_el_chall@hotmail.com>
2021-03-19 18:02:35 +0100

    serialize error api message and code

---
+++ src/EventSubscriber/ExceptionListener.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use JMS\Serializer\SerializerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ExceptionListener implements EventSubscriberInterface
+{
+    private $serializer;
+
+    public function __construct(SerializerInterface $serializer)
+    {
+        $this->serializer = $serializer;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        // return the subscribed events, their methods and priorities
+        return [
+            KernelEvents::EXCEPTION => [
+                ['processException', 10],
+                ['logException', 0],
+                ['notifyException', -10],
+            ],
+        ];
+    }
+
+    public function processException(ExceptionEvent $event)
+    {
+        $result = [];
+        $result['code'] = Response::HTTP_BAD_REQUEST;
+
+        $result['body'] = [
+            'code'    => Response::HTTP_BAD_REQUEST,
+            'message' => $event->getThrowable()->getMessage()
+        ];
+
+        $body = $this->serializer->serialize($result['body'], 'json');
+
+        $event->setResponse(new Response($body, $result['code']));
+    }
+
+    public function logException(ExceptionEvent $event)
+    {
+        // ...
+    }
+
+    public function notifyException(ExceptionEvent $event)
+    {
+        // ...
+    }
+}
