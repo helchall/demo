2157bb4882e344496bbcb5f64d2c200e51be63f5
helchall <h_el_chall@hotmail.com>
2021-03-27 16:04:40 +0100

    Adding Comment to rep log script

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 
-const el = <h2>Lift Stuff! <span>❤️</span></h2>;
 
-console.log(el);
-ReactDom.render(el, document.getElementById('lift-stuff-app'));
+class RepLogAPP extends Component {
+    render() {
+        return <h2>Lift Stuff! <span>❤️</span></h2>;
+    }
+}
+
+console.log(<RepLogAPP/>)
+
+ReactDom.render(<RepLogAPP/>, document.getElementById('lift-stuff-app'));
