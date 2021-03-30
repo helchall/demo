d9764ad725fd5d11faf16e68492d85df571895b6
helchall <h_el_chall@hotmail.com>
2021-03-30 18:25:07 +0200

    Updating app.js demo access public and private api with react

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,39 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
 
+// import React from 'react';
+// import { render } from 'react-dom';
+// import NewUSer from './components/NewUser';
+// import { BrowserRouter } from 'react-router-dom';
+
+// render(<BrowserRouter>
+//     <NewUSer />
+//   </BrowserRouter>, document.getElementById('new_user'));
+
+
+// assets/js/app.js
+
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import Home from "./components/Home";
+
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Home/>
+                </div>
+            </BrowserRouter>
+        )
+    }
+}
+
+ReactDom.render(<App />, document.getElementById('root'));
+
+
+
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
