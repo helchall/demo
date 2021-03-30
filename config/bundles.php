6c946dce82f9ffc5ec6a05d9ffe55939e4020beb
helchall <h_el_chall@hotmail.com>
2021-03-30 18:26:16 +0200

    Ading auth0 jwt bundle and adapting security and service yaml

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Auth0\JWTAuthBundle\JWTAuthBundle::class => ['all' => true],
 ];
