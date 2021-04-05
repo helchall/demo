83311485d44c0da5e29c3a192448453e9f733753
helchall <h_el_chall@hotmail.com>
2021-04-05 18:59:24 +0200

    Adding react comedy scripts

---
+++ assets/js/ReactComdedyClub/src/App.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function Joke(props) {
+  return (
+    <div className="joke">
+      <p className="setup">{props.joke.setup}...</p>
+      <p className="punchline">{props.joke.punchline}</p>
+    </div>
+  );
+}
+
+function App() {
+  const [joke, setJoke] = useState({
+    setup: ``,
+    punchline: ``
+  });
+
+  useEffect(() => {
+    fetchJoke();
+  }, []);
+
+  function fetchJoke() {
+    fetch("https://official-joke-api.appspot.com/jokes/programming/random")
+      .then(resp => resp.json())
+      .then(data => setJoke(data[0]));
+  }
+
+  return (
+    <div className="App">
+      <Joke joke={joke} />
+      <button className="btn" onClick={() => fetchJoke()}>
+        another one
+      </button>
+      <a
+        className="tweet btn"
+        href={
+          "https://twitter.com/intent/tweet?text=" +
+          joke.setup +
+          " " +
+          joke.punchline +
+          " &via=alioukahere&hashtags=reactcomedyclub,kaherecode"
+        }
+        target="_blank"
+      >
+        tweet
+      </a>
+      <hr/>
+      <div className="back-dashboard">
+        <a
+          className="btn btn-secondary"
+          href={
+            "http://demo.local/dashboard/"
+          }
+        >
+          dashboard
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
