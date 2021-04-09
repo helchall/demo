2cfe6191d205489c3d3068d8f70804fa74de8f46
helchall <h_el_chall@hotmail.com>
2021-04-09 18:43:55 +0200

    Adding new type of  user form

---
+++ assets/js/RepLog/indexUserForm1.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import DemoForm from './DemoForm';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div className="container">
+       <DemoForm />
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
