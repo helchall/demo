52082dfa8359c2048c2bdd6deed80f81aaff1ba5
helchall <h_el_chall@hotmail.com>
2021-04-09 18:44:14 +0200

    Adding validation to userForm script

--- assets/js/RepLog/UserForm.js
+++ assets/js/RepLog/UserForm.js
@@ -10,12 +10,14 @@ export default class UserForm extends Component {
             email: '',
             username: '',
             plainPassword: '',
+            confirmPassword: '',
             errorMessage: '',
             error: false,
+            errors: {},
             isLoading: false
         }
         this.handleChange = this.handleChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
+        this.onSubmit     = this.onSubmit.bind(this);
     }
 
     handleChange(e) {
@@ -27,39 +29,91 @@ export default class UserForm extends Component {
 
     async onSubmit(e) {
         e.preventDefault();
-        this.setState({
-            isLoading: true,
-            error: false,
-            errorMessage: ''
-        });
-
-        const response = await fetch(API_BASE_URL + '/user/new/true', {
-            method: 'POST',
-            body: JSON.stringify({
-                "email": this.state.email,
-                "username": this.state.username,
-                "plainPassword": this.state.plainPassword
-            })
-        });
-        const data = await response.json();
-
-        if (data.errors) {
+        if(this.validate()) {
             this.setState({
-                isLoading: false,
-                error: true,
-                errorMessage: data.errors
-            });
-        } else {
-            this.setState({
-                email: '',
-                username: '',
-                plainPassword: '',
-                isLoading: false,
+                isLoading: true,
                 error: false,
                 errorMessage: ''
             });
-            this.props.onAddition(data);
+
+            const response = await fetch(API_BASE_URL + '/user/new/true', {
+                method: 'POST',
+                body: JSON.stringify({
+                    "email": this.state.email,
+                    "username": this.state.username,
+                    "plainPassword": this.state.plainPassword
+                })
+            });
+            const data = await response.json();
+
+            if (data.errors) {
+                this.setState({
+                    isLoading: false,
+                    error: true,
+                    errorMessage: data.errors
+                });
+            } else {
+                this.setState({
+                    email: '',
+                    username: '',
+                    plainPassword: '',
+                    confirmPassword: '',
+                    isLoading: false,
+                    error: false,
+                    errorMessage: ''
+                });
+                this.props.onAddition(data);
+            }
         }
+    }
+
+    validate(){
+        let input = this.state;
+        let errors = {};
+        let isValid = true;
+
+        if (!input["username"]) {
+          isValid = false;
+          errors["username"] = "Please enter your username.";
+        }
+
+        if (!input["email"]) {
+          isValid = false;
+          errors["email"] = "Please enter your email Address.";
+        }
+
+        if (typeof input["email"] !== "undefined") {
+
+          var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+          if (!pattern.test(input["email"])) {
+            isValid = false;
+            errors["email"] = "Please enter valid email address.";
+          }
+        }
+
+        if (!input["plainPassword"]) {
+          isValid = false;
+          errors["plainPassword"] = "Please enter your password.";
+        }
+
+        if (!input["confirmPassword"]) {
+          isValid = false;
+          errors["confirmPassword"] = "Please enter your confirm password.";
+        }
+
+        if (typeof input["plainPassword"] !== "undefined" && typeof input["confirmPassword"] !== "undefined") {
+
+          if (input["plainPassword"] != input["confirmPassword"]) {
+            isValid = false;
+            errors["plainPassword"] = "Passwords don't match.";
+          }
+        }
+
+        this.setState({
+          errors: errors
+        });
+
+        return isValid;
     }
 
     render() {
@@ -68,16 +122,56 @@ export default class UserForm extends Component {
                 <h4>Create User</h4>
                 <Form error={this.state.error} onSubmit={this.onSubmit}>
                     <Form.Field error={this.state.error}>
-                        <label>Email</label>
-                        <input placeholder='enter email' name="email" required="required" value={this.state.email} onChange={this.handleChange}/>
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="text"
+                            name="email"
+                            value={this.state.email}
+                            onChange={this.handleChange}
+                            className="form-control"
+                            placeholder="Enter email"
+                            id="email" />
+
+                            <div className="text-danger">{this.state.errors.email}</div>
                     </Form.Field>
                     <Form.Field error={this.state.error}>
-                        <label>Username</label>
-                        <input placeholder='enter username' name="username" required="required" value={this.state.username} onChange={this.handleChange}/>
+                        <label htmlFor="username">Username</label>
+                        <input
+                            type="text"
+                            name="username"
+                            value={this.state.username}
+                            onChange={this.handleChange}
+                            className="form-control"
+                            placeholder="Enter username"
+                            id="username" />
+
+                            <div className="text-danger">{this.state.errors.username}</div>
                     </Form.Field>
                     <Form.Field error={this.state.error}>
-                        <label>Password</label>
-                        <input placeholder='enter password' name="plainPassword" required="required" value={this.state.plainPassword} onChange={this.handleChange}/>
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            name="plainPassword"
+                            value={this.state.plainPassword}
+                            onChange={this.handleChange}
+                            className="form-control"
+                            placeholder="Enter password"
+                            id="plainPassword" />
+
+                            <div className="text-danger">{this.state.errors.plainPassword}</div>
+                    </Form.Field>
+                    <Form.Field error={this.state.error}>
+                        <label htmlFor="password">Confirm Password</label>
+                        <input
+                            type="password"
+                            name="confirmPassword"
+                            value={this.state.confirmPassword}
+                            onChange={this.handleChange}
+                            className="form-control"
+                            placeholder="Enter confirm password"
+                            id="confirmPassword" />
+
+                            <div className="text-danger">{this.state.errors.confirmPassword}</div>
                     { this.state.error &&
                     <Message
                         error
@@ -86,7 +180,7 @@ export default class UserForm extends Component {
                     />
                     }
                     </Form.Field>
-                    <Button className="btn btn-primary btn-user" type='submit' loading={this.state.isLoading}>Add User</Button>
+                    <Button style={{marginTop:8}} className="btn btn-success" type='submit' loading={this.state.isLoading}>Add User</Button>
                 </Form>
             </div>
         )
