dd773e74c3347fa202ef15ab46352d52f8d88137
helchall <h_el_chall@hotmail.com>
2021-04-11 18:18:27 +0200

    Adding testpagination script to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,7 @@ Encore
     .addEntry('index', './assets/js/index.js')
     .addEntry('indextoggle', './assets/js/hooks/index.js')
     .addEntry('index_react_comedy', './assets/js/ReactComdedyClub/src/index.js')
+    .addEntry('testpagination', './assets/js/pagination/TodoApp.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
